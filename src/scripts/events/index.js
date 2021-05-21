201e86a95a15cddf1196af9e1ec69667a626fe21
marioecg <marioecg@gmail.com>
2021-05-21 15:02:04 -0600

    remove mouse event

--- src/scripts/events/index.js
+++ src/scripts/events/index.js
@@ -1,6 +1,5 @@
 import Events from './Emitter';
 import GlobalRaf from './Raf';
 import GlobalResize from './Resize';
-import GlobalMouse from './Mouse';
 
-export { Events, GlobalRaf, GlobalResize, GlobalMouse };
+export { Events, GlobalRaf, GlobalResize };
